d2cbfe70592583517b114d2c8be10b41244cb1f7
Ludmila Ramirez <ludmil4.ramirez@gmail.com>
2026-05-15 22:22:59 -0300

    Cambios en el CORS

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,8 @@ const PORT = process.env.PORT || 3001;
 app.use(cors({
     origin: [
         "http://localhost:5173",
-        "https://medturno-farpebvjz-ludmirzs-projects.vercel.app"
+        "https://medturno.vercel.app",
+        /\.vercel\.app$/
     ]
 }));
 
